a9c482434cbb49c1c79b9c6b7b638b3fae65d2dc
George Capraroiu <georgecapraroiu1205@gmail.com>
2020-03-20 02:24:04 +0700

    404 page updated

---
+++ src/views/backend/errors/page-404.jsx
@@ -0,0 +1,14 @@
+import React, {Component, Fragment} from 'react';
+
+class Page404 extends Component {
+    render() {
+        return (
+            <Fragment>
+
+            </Fragment>        
+            );
+
+    }
+}
+
+export default Page404;
